2b9e1f7ee967d52dbd5c2dc3a9065c7aae9089b5
Matthew Nutter <matthew.s.nutter@gmail.com>
2019-03-27 18:11:52 +0000

    Update-FreshserviceRequester Added

--- FreshserviceRequesters.ps1
+++ FreshserviceRequesters.ps1
@@ -10,7 +10,25 @@ function Get-FreshserviceRequesters
     return $response
 }
 
-function Update-FreshserviceRequester {}
+function Update-FreshserviceRequester {
+    param (
+        [int64]$id,  #todo : make mandatory
+        [string]$first_name,
+        [string]$last_name,
+        [string]$location_id,
+        [string]$job_title
+    )
+    $body = @{
+        first_name = $first_name
+        last_name = $last_name
+        location_id = $location_id
+        job_title = $job_title
+    }
+    ($body.GetEnumerator() | Where-Object { -not $_.Value }) | ForEach-Object { $body.Remove($_.Name) } # remove empty values from hashtable
+    $body = $body | ConvertTo-Json
+    $path = '/api/v2/requesters/' + $id
+    Invoke-FreshserviceRestMethod -Method PUT -Path $path -Body $body
+}
 
 function New-FreshserviceRequester {}
 
